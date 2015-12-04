c7f0fedd0a7e80108c518dc5b585c7c9e1bce8ad
roots4all <amandahjensen@gmail.com>
2015-12-04 04:04:48 -0600

    Initial commit.

---
+++ storagerepos.js
@@ -0,0 +1,28 @@
+//singleton instance object...
+var RepositoryManager = (function () {
+  var instance;
+
+  function createRepositoryObject() {
+      return {
+        set: function (key, value) {
+          window.localStorage.setItem(key, value);
+        },
+        get: function (key) {
+          return window.localStorage.getItem(key);
+        }
+      };
+  };
+
+  return {
+    // get instance of Repository
+    getInstance: function () {
+      // if instance doesn't already exist - create
+      if (!instance) {
+        instance = createRepositoryObject();
+      }
+      // otherwise, return reference to existing instance
+      return instance;
+    }
+  };
+
+})();
